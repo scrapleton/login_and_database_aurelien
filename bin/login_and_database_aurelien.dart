c5b064eef59bd46277b5b27b6584f6afcdbbef45
scrapleton <wynant.a@gmail.com>
2024-08-21 16:03:45 +0200

    added comment

--- bin/login_and_database_aurelien.dart
+++ bin/login_and_database_aurelien.dart
@@ -41,17 +41,22 @@ appLoginProcess() {
   userPassword = stdin.readLineSync();
   stdin.echoMode = true;
 
+  //UserName is mandatory
   if (userName != null && userName!.isNotEmpty) {
+    //LOG AS ADMIN case
     if (userPassword == Data.adminPassword) {
       isUserAdmin = true;
       isUserLogged = true;
+      //LOG AS GEST case
     } else if (userPassword == Data.guestPassword) {
       isUserLogged = true;
+      //WRONG PASSWORD case
     } else {
       stdout.write("\x1B[2J\x1B[0;0H");
       stdout.write("\nWrong password");
       appTryAgainOrQuit();
     }
+    //NO USERNAME case
   } else {
     stdout.write("\x1B[2J\x1B[0;0H");
     stdout.write("\nYou did not enter any name ...");
@@ -69,17 +74,22 @@ appDataAccess() {
   stdout.write("\n\nType a user ID to access his data (ID are numbers) : ");
   String? index = stdin.readLineSync();
 
+  //Index must be a INT
   if (index != null && int.tryParse(index) != null) {
-    Map? selectedUser = Data.userInfoDatabase
+    //Get user or empty map
+    Map selectedUser = Data.userInfoDatabase
         .firstWhere((user) => user['id'] == int.parse(index), orElse: () => {});
 
+    //Show user Data
     if (selectedUser.isNotEmpty) {
       appShowUserData(selectedUser);
       appTryAgainOrQuit();
+      //No entry found
     } else {
       stdout.write("\nNO ENTRY FOUND");
       appTryAgainOrQuit();
     }
+    //Invalid ID
   } else {
     stdout.write("\nERROR : Invalid ID");
     stdout.write("\nID must be a number");
